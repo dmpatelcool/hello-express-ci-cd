7c57d937e696854d34efaf9b1f5a13d16ad07d0f
“dmpatelcool <dmpatelcool@gmail.com>
2026-03-23 15:54:18 +0530

    Initial hello world express with tests

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+// test/app.test.js
+const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+
+describe('GET /', () => {
+  it('should return Hello World on main page', async () => {
+    const res = await request(app).get('/');
+    assert.strictEqual(res.status, 200);
+    assert.strictEqual(res.text, 'Hello World');
+  });
+});
